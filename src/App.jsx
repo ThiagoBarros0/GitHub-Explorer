6ff2b1e0f7ebcc6b3bf46094e0cd3f3fb7973966
ThiagoBarros0 <thiago.barrosoliveira0@gmail.com>
2021-11-25 14:36:04 -0400

    First commit Fabio Class

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+import { GlobalStyle } from './styles/global';
+import {Container} from './styles/Home'
+import {SearchBar} from './components/SearchBar'
+import {User} from './components/User'
+import {ReposList} from './components/ReposList'
+
+function App() {
+  const[user,setUser] = useState()
+  const[userRepos, setUserRepos] = useState()
+
+
+  return (
+    <Container>
+      <SearchBar setUser={setUser} setUserRepos={setUserRepos}/>
+      {user && <User user={user} userRepos={userRepos} />}
+      <ReposList/>
+      <GlobalStyle />
+    </Container >
+  )
+}
+
+export default App;
---
+++ src/components/User/index.jsx
@@ -0,0 +1,27 @@
+import {Container} from './styles'
+
+export function User({user, userRepos}){
+    console.log(user)
+    return(
+        <Container>
+        <img src={user.avatar_url} alt={user.name} />
+        <h1>{user.name}</h1>
+        <h2>{user.login}</h2>
+        <p>{user.bio}</p>
+        <section>
+            <div>
+                <strong>{userRepos?.length}</strong>
+                <span>Repositórios</span>
+            </div>
+            <div>
+                <strong>{user.followers}</strong>
+                <span>Seguidores</span>
+            </div>
+            <div>
+                <strong>{user.following}</strong>
+                <span>Seguindo</span>
+            </div>
+        </section>
+        </Container>
+    )
+}
