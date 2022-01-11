d016935543fb059eb2e43e4785a71e8824934ff6
ThiagoBarros0 <thiago.barrosoliveira0@gmail.com>
2022-01-10 20:21:29 -0400

    Project Completed

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,5 +1,7 @@
 import { FiArrowLeft } from "react-icons/fi"
 import { Container } from "./styles"
+import { List } from "../Li"
+
 
 export function ReposList({user,userRepos, setIsReposListActive}) {
     return (
@@ -14,11 +16,12 @@ export function ReposList({user,userRepos, setIsReposListActive}) {
 
         <ul>
         {userRepos.map(repo => (
-                <li key={repo.id}>
-                <h2>{repo.name}</h2>
-                <p>{repo.description}</p>
-                <a href={repo.html_url}>Acessar repositório</a>
-            </li>
+            //      <li key={repo.id}>
+            //      <h2>{repo.name}</h2>
+            //      <p>{repo.description}</p>
+            //      <a href={repo.html_url}>Acessar repositório</a>
+            //  </li>
+            <List repo={repo}/>
             ))}
         </ul>
         </Container>
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import {FiSearch} from 'react-icons/fi'
 import { Container } from './style'
+import{toast,ToastContainer} from 'react-toastify'
 
 export function SearchBar({setUser, setUserRepos}){
     const [searchedValue, setSearchedValue] = useState('')
+    
+    
     
     function getUserData(e){
         e.preventDefault();
@@ -11,23 +14,57 @@ export function SearchBar({setUser, setUserRepos}){
         try{
             fetch(`http://api.github.com/users/${searchedValue}`)
             .then(response => response.json())
-            .then(data => setUser(data))
+            // .then(data => setUser(data))
+            .then(data =>{
+                if(data.message === 'Not Found'){
+                    setUser(null)
+                    setUserRepos(null)
 
-            fetch(`http://api.github.com/users/${searchedValue}/repos`)
-            .then(response => response.json())
-            .then(data => setUserRepos(data))
+                    // toast.error('🦄 Usuário não encontrado!', {
+                    //     position: "top-center",
+                    //     autoClose: 5000,
+                    //     hideProgressBar: false,
+                    //     closeOnClick: true,
+                    //     pauseOnHover: true,
+                    //     draggable: true,
+                    //     progress: undefined,
+                    //     });
+
+                } else{
+                    setUser(data)
+
+                    fetch(`http://api.github.com/users/${searchedValue}/repos`)
+                    .then(response => response.json())
+                    .then(data => setUserRepos(data))
+
+                }
+            })
+            
+            
+            
         }catch(error){
             console.log(error)
         }
     }
-    
+
+    // Tentativa
+    // function valueInput (input)  {
+    //     this.input = input;
+    //   };
+
     return (
         <Container>
             <h1>Github Explorer</h1>
             <form onSubmit={getUserData}>
                 <div>
                 <span>github.com/</span>
+                
+                {/* Forma correta */}
                 <input type="text" value={searchedValue} onChange={e => setSearchedValue(e.target.value)} />
+
+                {/* Tentativa */}
+                {/* <input ref={this.input} /> */}
+                
                 </div>
                 <button type='submit'>
                     <FiSearch />
